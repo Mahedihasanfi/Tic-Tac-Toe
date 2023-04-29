b4215179953a3d7333a7d1d76403abfae09e9448
Mahedi Hasan <mah.hasan26@hotmail.com>
2023-04-30 00:44:37 +0300

    Third  commit

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -1,2 +1,20 @@
-print("hello")
-this is to check
+def sign():
+    you = input("Welcome to tic-tac-toe, you take X or O:?  ")
+    if you == 'X':
+        comp = '0'
+    elif you == '0':
+        comp = 'X'
+    else:
+        print("Invalid input!")
+    print("you:",you)
+    print("comp:",comp)   
+    
+position = [(0,0),(0,1),(0,2),(1,0),(1,1),(1,2),(2,0),(2,1),(2,2)]
+position= ["-", "-", "-", "-", "-", "-","-", "-", "-"]
+def print_position():
+    print(position[0] + " | " + position[1] + " | " + position[2])
+    print(position[3] + " | " + position[4] + " | " + position[5])
+    print(position[6] + " | " + position[7] + " | " + position[8])
+
+sign()
+print_position()
